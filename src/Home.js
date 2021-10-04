01c9a1883bed97e8ec727b2c4493435de48e0993
Andrii Melnyk <melnik.andrey@gmail.com>
2021-10-04 15:58:13 -0400

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,80 @@
+function Home() {
+  return (
+    <div className="tj ph7 pv4 v-top">
+      <h1 className="tc">Simple-Market</h1>
+      <h1 className="tc">Information</h1>
+      <p className="tc">Last updated: September 30, 2021</p>
+      <p>
+        "Simple-Market"is a non-profit demo application with the main purpose to
+        demonstrate its functions, it is free and available to everyone older 13
+        years old, and assumes the use of third-party tools and technologies in
+        processing, such as authentication and storage information. "Simple
+        Market".
+        {"\n"} As a demo application all data is only for demonstation purpose
+        and could not be treated as a real or valid. {"\n"} As a user you can:
+      </p>
+      <ul>
+        <li>
+          <p>
+            <strong>Create</strong> an account;
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Log In/Out</strong> with an account;
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Reset</strong> password;
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Access</strong> to all available demo products;
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Make</strong> demo orders;
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Create/Remove</strong> your own products that will be
+            available for all users.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Add</strong> to favourite any product.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Service</strong> refers to the Application.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Add photo/image</strong> to your products.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Verify</strong> previuos orders
+          </p>
+        </li>
+      </ul>
+      <h2>Contact Us</h2>
+      <p>
+        For any questions or feedback about the Application, You can contact us
+      </p>
+      <ul>
+        <li>By email: andriimelnyk.native@gmail.com</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
